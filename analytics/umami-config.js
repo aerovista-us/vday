eacb6758da2ef7e5aa1149417ce3d030db9cc221
AeroVista <aerovistaus@gmail.com>
2026-02-05 00:48:14 -0800

    Remove umami-loader.js configuration

---
+++ analytics/umami-config.js
@@ -0,0 +1,6 @@
+// analytics/umami-config.js
+// Edit these two values per project/environment.
+window.__UMAMI__ = {
+  url: "http://100.115.9.61:8787",          // later: https://stats.yourdomain.com
+  websiteId: "PASTE_WEBSITE_ID_HERE"
+};
